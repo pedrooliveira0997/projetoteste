2f25fcbefdcbfba8e00d94d006746028ff43dab8
Pedro Henrique Oliveira Lima <pedrooliveira0997@gmail.com>
2021-11-01 16:58:02 -0300

    enviar projeto

---
+++ src/test/java/com/projetoteste/projetoteste/ProjetotesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoteste.projetoteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetotesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
