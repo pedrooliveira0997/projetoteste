d426ca613f5d5bb92369c6d8af045d20b9d46529
pedrooliveira0997 <42185577+pedrooliveira0997@users.noreply.github.com>
2021-11-02 22:02:44 -0300

    Update IndexController.java

--- src/main/java/com/projetoteste/projetoteste/IndexController.java
+++ src/main/java/com/projetoteste/projetoteste/IndexController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 	
-	@RequestMapping("/")
+	@RequestMappinig("/")
 	public String index() {
-		return "indexx";
+		return "index";
 		
 	}
 
